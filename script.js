179036b7fba6ba6f3212fc6a100f88fd708a4122
Aliceman93 <153220998+Aliceman93@users.noreply.github.com>
2023-12-24 15:37:00 +0300

    Домашнее задание 4
    Маша, я видимо, сделала что-то не так решила тебя не беспокоить перед праздником и не писать в ТГ

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 /* Новые элементы должны добавляться в список по нажатию на Enter */
+const sendInput = document.querySelector('#input');
+const groceriesContainer = document.querySelector('.groceries')
 
+sendInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const groceriesText = sendInput.value;
+    }
+
+    const newGroceries = document.createElement('div');
+    newGroceries.classList.add('text');
+    newGroceries.textContent = groceriesText;
+
+    const listElement = document.querySelectorAll('div');
+    for (div of listElement) {
+        div.addEventListener('click', function() {
+            div.classList.toggle('done');
+        });
+    };
+
+    if (groceriesText != '') {
+        groceriesContainer.append(groceriesText);
+    }
+
+    sendInput.value = '';
+});
 /* Пустые элементы не должны добавляться */
 
 /* Если кликнуть на элемент списка, он зачеркивается */
